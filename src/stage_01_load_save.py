b1f14f6ee74a677b537b25a315021f1b83829eec
bhagwat-chate <bhagwat.chate25@gmail.com>
2021-10-02 10:47:59 +0530

    stage 01 added to dvc.yaml

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -22,7 +22,7 @@ def get_data(config_path):
     raw_local_file_path = os.path.join(raw_local_dir_path, raw_local_file)
 
     df.to_csv(raw_local_file_path, sep=",", index=False)
-
+    # print(df.head())
 
 
 if __name__ == '__main__':
